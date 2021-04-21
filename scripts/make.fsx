db66702df0c5782356cf43d15a080a72c34d56b0
Andrew Cann <shum@canndrew.org>
2021-04-21 13:14:18 +0800

    End2End: introduce revocation testing

--- scripts/make.fsx
+++ scripts/make.fsx
@@ -318,6 +318,9 @@ let RunTests (suite: string) =
             "ChannelLocalForceClosing"
             "ChannelRemoteForceClosingByFunder"
             "ChannelRemoteForceClosingByFundee"
+(* not ready yet:
+            "Revocation"
+*)
         ]
 
     let geewallet2geewalletTestNamePrefix, funderSuffix, fundeeSuffix = "G2G_", "_Funder", "_Fundee"
