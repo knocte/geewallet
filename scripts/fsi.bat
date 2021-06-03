144dbff02184b7c2ada7d4c9559964ae8014b42e
Andres G. Aragoneses <knocte@gmail.com>
2021-06-03 15:07:39 +0800

    test

--- scripts/fsi.bat
+++ scripts/fsi.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 
-SET ENTERPRISE="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\CommonExtensions\Microsoft\FSharp\Tools\fsi.exe"
+SET ENTERPRISE="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\CommonExtensions\Microsoft\FSharp\Toolz\fsi.exe"
 SET ENTERPRISE_OLD="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\CommonExtensions\Microsoft\FSharp\fsi.exe"
-SET COMMUNITY="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\FSharp\Tools\fsi.exe"
+SET COMMUNITY="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\FSharp\Toolz\fsi.exe"
 SET COMMUNITY_OLD="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\FSharp\fsi.exe"
-SET BUILDTOOLS="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\BuildTools\Common7\IDE\CommonExtensions\Microsoft\FSharp\Tools\fsi.exe"
+SET BUILDTOOLS="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\BuildTools\Common7\IDE\CommonExtensions\Microsoft\FSharp\Toolz\fsi.exe"
 SET BUILDTOOLS_OLD="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\BuildTools\Common7\IDE\CommonExtensions\Microsoft\FSharp\fsi.exe"
 
 IF EXIST %ENTERPRISE% (
@@ -26,6 +26,9 @@ IF EXIST %ENTERPRISE% (
                         SET RUNNER=%BUILDTOOLS_OLD%
                     ) ELSE (
                         ECHO fsi.exe not found, is F# installed?
+                        CD %ProgramFiles(x86)%
+                        DIR /S fsi.exe
+                        ECHO Or maybe it changed location, see above if you find it.
                         EXIT /b 1
                     )
                 )
